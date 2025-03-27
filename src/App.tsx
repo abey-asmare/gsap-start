268eae7a32455ec5dc1131eeac63e45c875d7c89
abey asmare <abeyasmare07@gmail.com>
2025-03-27 17:38:23 +0300

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,6 @@ function App() {
       <div className="boxContainer flex gap-8 m-8" ref={boxRef}>
         <div className="box w-16 h-16 bg-cyan-500 rounded-xl block"></div>
       </div>
-      <div>hello</div>
     </div>
   );
 }
